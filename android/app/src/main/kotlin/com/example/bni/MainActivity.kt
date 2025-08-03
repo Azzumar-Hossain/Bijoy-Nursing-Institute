b6c81f3ef51774796063260341659fdaa365cdf9
Azzumar Hossain <73302892+Azzumar-Hossain@users.noreply.github.com>
2025-08-03 12:00:17 +0600

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/bni/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bni
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/providers/exam_terms_provider.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+import 'package:bni/models/exam_term_model.dart';
+import 'package:bni/models/result_params_model.dart';
+import 'package:bni/models/term_result_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+
+final examTermsProvider = FutureProvider.family<List<ExamTerm>, String>((
+  ref,
+  userName,
+) async {
+  final response = await http.get(
+    Uri.parse(
+      'http://203.190.12.69/ems/bni/app/web_api/std_exam_results/std_progress_report_api.php?user_name=$userName',
+    ),
+  );
+
+  if (response.statusCode != 200) {
+    throw Exception("Failed to fetch terms");
+  }
+
+  final data = jsonDecode(response.body);
+  return (data['data']['available_exam_type'] as List)
+      .map((e) => ExamTerm.fromJson(e))
+      .toList();
+});
+
+final resultProvider = FutureProvider.family<TermResult?, ResultParams>((
+  ref,
+  params,
+) async {
+  final userName = params.userName;
+  final selectedTermId = params.selectedTermId;
+
+  final uri =
+      Uri.parse(
+        'http://203.190.12.69/ems/bni/app/web_api/std_exam_results/std_progress_report_api.php',
+      ).replace(
+        queryParameters: {
+          'user_name': userName,
+          'exam_session': '2025',
+          'exam_type[]': selectedTermId,
+        },
+      );
+
+  final response = await http.get(uri);
+
+  if (response.statusCode != 200) {
+    throw Exception('Failed to fetch result');
+  }
+
+  final data = jsonDecode(response.body);
+  final resultTest = data['data']['result_test'];
+  if (resultTest == null || resultTest is! List) {
+    throw Exception('Invalid result data');
+  }
+
+  final resultList = resultTest
+      .map<TermResult>((e) => TermResult.fromJson(e))
+      .toList();
+
+  return resultList
+          .where((term) => term.termId.toString() == selectedTermId)
+          .isNotEmpty
+      ? resultList.firstWhere(
+          (term) => term.termId.toString() == selectedTermId,
+        )
+      : null;
+});
